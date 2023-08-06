df7e3aeb36d04c554ae829676a0b0576406b5ac8
Marina Lyassova <132856322+lma2023@users.noreply.github.com>
2023-08-05 18:10:03 -0700

    Adding the MOVES constant

--- constants.js
+++ constants.js
@@ -1,9 +1,19 @@
 const HOST = 'localhost';
 const PORT = 50541;
-const NAME = "Name: Marina";
+const NAME = "Name: LM";
+const MOVES = {
+  w: "Move: up",
+  a: "Move: left",
+  s: "Move: down",
+  d: "Move: right",
+  q: "Say: Hello",
+  e: "Say: I'm having fun!",
+  r: "Say: Good Bye!"
+};
 
 module.exports = {
   HOST,
   PORT,
-  NAME
+  NAME,
+  MOVES
 };
